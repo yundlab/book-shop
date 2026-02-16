e2cf739e16448e3b8d9d76811bd336bd791fa073
yundlab <by.yundlab@gmail.com>
2026-02-16 13:01:20 +0900

    category api

---
+++ data.sql
@@ -0,0 +1,11 @@
+INSERT INTO books (title, form, isbn, summary, datail, author, pages, contents, price, pub_date)
+VALUES ('어린왕자', '종이책', 0, '어린왕..', '보아뱀..', '김왕자', '100', '목차입니다', '20000', '2019-01-01');
+
+INSERT INTO books (title, form, isbn, summary, datail, author, pages, contents, price, pub_date)
+VALUES ('신데렐라', '종이책', 1, '신데렐..', '유리구두..', '김구두', '100', '목차입니다', '20000', '2023-12-01');
+
+INSERT INTO books (title, form, isbn, summary, datail, author, pages, contents, price, pub_date)
+VALUES ('백설공주', '종이책', 2, '백설공..', '사과..', '김마녀', '100', '목차입니다', '20000', '2026-01-20');
+
+INSERT INTO books (title, form, isbn, summary, datail, author, pages, contents, price, pub_date)
+VALUES ('데미안', '종이책', '0', '데미안..', '사랑..', '싱클레어', '100', '목차입니다', '20000', '2026-02-01');
